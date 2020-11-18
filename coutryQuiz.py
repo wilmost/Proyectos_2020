5d00032ee8e8e816b6839fdadbe4a2cb296d1fd6
wilmost <66658857+wilmost@users.noreply.github.com>
2020-11-18 06:39:05 -0400

    Create coutryQuiz.py

---
+++ coutryQuiz.py
@@ -0,0 +1,34 @@
+
+import json
+import requests
+
+
+
+class Quiz:  
+    def __init__(self,question,): 
+        self.question =question 
+        self.answer = answer 
+        self.num_question= num_question
+        self.opcions= options 
+
+    
+    def play(self):
+        pass 
+
+
+    def result(self): 
+        pass
+    
+
+
+class Countries: 
+    def __init__(self, country,city):
+         self.country= country 
+         self.city = city 
+    
+    
+
+
+spain = Countries("spain", 'madrid') 
+
+print(spain.city)
